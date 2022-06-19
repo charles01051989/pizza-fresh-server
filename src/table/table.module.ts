8ac084a5ccbd3e800f9564e4a38aafa3309ad9c9
Charles Marques <charles_sjp@yahoo.com.br>
2022-06-19 12:16:12 -0300

    feat(table): create 'Service'

--- src/table/table.module.ts
+++ src/table/table.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TableService } from './tabble.service';
 import { TableController } from './table.controller';
 
 @Module({
   controllers: [TableController],
-  providers: [],
+  providers: [TableService],
 })
 export class TableModule {}
