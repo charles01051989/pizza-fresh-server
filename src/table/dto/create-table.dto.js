13253e448c61fbb5cb4e195a388be2c9f7b07351
Charles Marques <charles_sjp@yahoo.com.br>
2022-06-20 18:26:33 -0300

    feat(table): implement create table endpoint

---
+++ src/table/dto/create-table.dto.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.CreateTableDto = void 0;
+var CreateTableDto = /** @class */ (function () {
+    function CreateTableDto() {
+    }
+    return CreateTableDto;
+}());
+exports.CreateTableDto = CreateTableDto;
+;
---
+++ src/table/dto/entities/table.entity.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Table = void 0;
+var Table = /** @class */ (function () {
+    function Table() {
+    }
+    return Table;
+}());
+exports.Table = Table;
+;
