dd65cbed9acdc701fdd7977bd8c0cd802cb56fee
Charles Marques <charles_sjp@yahoo.com.br>
2022-07-01 23:18:46 -0300

    feat(product) dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNumber, IsString, IsUrl } from "class-validator"
+
 export class CreateProductDto{
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Mussarela',
+  })
+  @IsString()
   name: string
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: 'Queijo mussarela fino do fino, massa fina e borda recheada de catupiry'
+  })
+  @IsString()
   description: string
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: 12.34
+  })
+  @IsNumber({
+    maxDecimalPlaces:2,
+  })
   price: number
+  @ApiProperty({
+    description: 'Url da imagem',
+  })
+  @IsUrl()
   image: string
 }
